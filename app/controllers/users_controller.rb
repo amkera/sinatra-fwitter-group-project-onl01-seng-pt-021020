2774a6930f1daae3f44bb6482dd98cea2f7a446f
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-17 21:56:30 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
     
     get "/users/:slug" do 
       @user = User.find_by_slug(params[:slug])
-      binding.pry
+      erb :'/users/show_users'
     end 
     
 end
