75502b8f65b2381bde00b85e4afcf0be6e0ec794
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-17 22:16:26 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -42,5 +42,18 @@ class TweetsController < ApplicationController
       redirect to '/login'
     end
   end
+  
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
 end
