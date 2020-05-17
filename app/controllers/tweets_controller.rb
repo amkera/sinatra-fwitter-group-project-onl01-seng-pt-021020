ac5f0a86672db2ec0020c6e44d6c89b4cfdbfc53
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-17 22:15:06 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,5 +8,13 @@ class TweetsController < ApplicationController
       redirect '/login'
     end 
   end 
+  
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
 
 end
